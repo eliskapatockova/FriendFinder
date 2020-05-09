4fb92f25bb2077d683f9892e080cf8cd6c86f0ef
Eliska Patockova <patockovaeliska@gmail.com>
2020-05-09 11:21:48 -0700

    added images and set up friend options

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friendOptions = [
+    {
+        name: Katie,
+        photo: "Katie.jpg",
+        scores: [1, 3, 5, 3, 3, 2]
+    },
+
+    {
+        name: Emma,
+        photo: "/Emma.jpg",
+        scores: [1, 1, 2, 5, 5, 1]
+    },
+
+    {
+        name: Lindsay,
+        photo: "/Lindsay.jpg",
+        scores: [4, 3, 3, 1, 1, 1]
+    },
+
+    {
+        name: John,
+        photo: "/John.jpg",
+        scores: [3, 4, 5, 1, 4, 5]
+    },
+
+    {
+        name: Peter,
+        photo: "/Peter.jpg",
+        scores: [3, 1, 1, 3, 2, 4]
+    }
+
+]
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+//DONE
+
 var express = require("express");
 var path = require("path");
 
 var app = express();
 var bodyParser = require("body-parser");
 
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +14,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js");
+require("./app/routing/htmlRoutes.js");
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
