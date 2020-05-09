6c0c894b86199d5045b12b22315a18d1588b8b93
Eliska Patockova <patockovaeliska@gmail.com>
2020-05-09 08:56:46 -0700

    created dependencies and routing

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
+var bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
